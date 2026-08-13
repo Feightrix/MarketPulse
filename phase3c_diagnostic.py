761298a53b4a8255abd34f7f3a61261d9019e516
Aaron Feight <apf8.graphics@gmail.com>
2026-08-13 03:14:38 -0400

    Expose raw regime and date mapping diagnostics

--- phase3c_diagnostic.py
+++ phase3c_diagnostic.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+import pandas as pd
 import phase3c_regime_backtest as b
 
 
@@ -7,7 +8,32 @@ def main():
     data = b.load_data()
     regimes, selections = b.daily_regimes(data)
     dev = b.pack_period(data, regimes, selections, "2021-01-01", "2023-12-31")
-    out = {"regime_true_bars": {}, "regime_true_days": {}, "selection_counts": {}, "signal_counts": {}, "intersections": {}}
+    out = {
+        "raw_regime_true_days": {}, "raw_selection_counts": {}, "mapping_samples": {},
+        "regime_true_bars": {}, "regime_true_days": {}, "selection_counts": {},
+        "signal_counts": {}, "intersections": {}
+    }
+
+    for name, series in regimes.items():
+        out["raw_regime_true_days"][name] = int(series.fillna(False).sum())
+    for lb, mapping in selections.items():
+        counts = {s: sum(v == s for v in mapping.values()) for s in b.SYMBOLS}
+        counts["none"] = sum(v is None for v in mapping.values())
+        out["raw_selection_counts"][str(lb)] = counts
+
+    first_regime_key = next(iter(regimes.values())).index[0]
+    first_selection_key = next(iter(selections.values())).keys().__iter__().__next__()
+    first_pack_ns = int(dev["day_ns"][0])
+    out["mapping_samples"] = {
+        "regime_key_str": str(first_regime_key),
+        "regime_key_ns": int(pd.Timestamp(first_regime_key).value),
+        "selection_key_str": str(first_selection_key),
+        "selection_key_ns": int(pd.Timestamp(first_selection_key).value),
+        "pack_day_ns": first_pack_ns,
+        "pack_day_as_utc": str(pd.Timestamp(first_pack_ns, tz="UTC")),
+        "regime_key_matches_pack_first": int(pd.Timestamp(first_regime_key).value) == first_pack_ns,
+        "selection_key_matches_pack_first": int(pd.Timestamp(first_selection_key).value) == first_pack_ns,
+    }
 
     unique_days = dev["day_ns"]
     day_first = np.r_[True, unique_days[1:] != unique_days[:-1]]
